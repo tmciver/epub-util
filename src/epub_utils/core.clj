18dcfb6d9ed7ffa672f96b2ce1aa08366a8a94dd
Tim McIver <tim@timmciver.com>
2012-03-06 00:13:11 -0500

    Added a 'directly-inferior' function that will be used to split a seq of enlive-extracted html headings into groups that form a hierarchy. Added test for this function.

--- src/epub_utils/core.clj
+++ src/epub_utils/core.clj
@@ -7,6 +7,24 @@
 (defn headings [page]
   (html/select page #{[:h1] [:h2] [:h3] [:h4] [:h5]}))
 
+(defn- heading-to-val
+  "Returns a value corresponding to the heading ordinal, e.g. :h2 -> 2"
+  [h]
+  (->> h
+       name
+       second
+       str
+       Integer.))
+
+(defn directly-inferior?
+  "Returns true if the second entry represents a heading that is directly
+inferior to the first heading."
+  [h1 h2]
+  (let [h1val (heading-to-val h1)
+        h2val (heading-to-val h2)]
+    (and (< h1val h2val)
+         (= (- h2val h1val) 1))))
+
 (defn heading-to-navpoint
   "Converts a map representing an Enlive heading tag to a map representing an
   Enlive ePUB TOC NavPoint tag."
@@ -16,4 +34,5 @@
               :content [{:tag :text
                          :content (:content h)}]}
              {:tag :content
-              :attrs {:src ""}}]}) 
+              :attrs {:src ""}}]})
+
--- test/epub_utils/test/core.clj
+++ test/epub_utils/test/core.clj
@@ -1,6 +1,9 @@
 (ns epub-utils.test.core
-  (:use [epub-utils.core])
-  (:use [clojure.test]))
+  (:use [epub-utils.core]
+        [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest test-directly-inferior?
+  (are [h1 h2 res] (= (directly-inferior? h1 h2) res)
+       :h1 :h2 true
+       :h2 :h1 false
+       :h1 :h3 false))
