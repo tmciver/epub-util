8f8b404056c1d83aecf8f95538397a60d6f2a7f1
Tim McIver <tim@timmciver.com>
2012-03-10 16:14:25 -0500

    Created function 'split-when-not-equal'.

--- src/epub_utils/core.clj
+++ src/epub_utils/core.clj
@@ -25,6 +25,17 @@ subordinate or equal to the first entry."
     (or (= (- h2val h1val) 1)
         (= h2val h1val))))
 
+(defn split-when-not-equal
+  "Splits the given collection at the point at which successive elements become
+  unequal. For example, [:a :a :b :c] becomes [[:a :a] [:b :c]] and [:a :b :c]
+  becomes [[:a] [:b :c]]."
+  [coll]
+  (loop [l (vector (first coll))
+         r (rest coll)]
+    (if (and (not (empty? r)) (= (last l) (first r)))
+      (recur (conj l (first r)) (rest r))
+      [l (vec r)])))
+
 (defn heading-to-navpoint
   "Converts a map representing an Enlive heading tag to a map representing an
   Enlive ePUB TOC NavPoint tag."
@@ -36,3 +47,17 @@ subordinate or equal to the first entry."
              {:tag :content
               :attrs {:src ""}}]})
 
+(defn headings-to-navpoints
+  "Converts a seq of enlive html headings and returns a nested map structure
+  of navpoint maps."
+  [hs]
+  )
+
+(defn fatten
+  "Opposite of flatten. ;)  Takes a flat seq and constructs a nested vector such
+  that a member 'b' of the seq is a child of the preceding member 'a' if
+  subordinate? returns non-nil and consecutive members are considered siblings
+  if siblings? returns non-nil."
+  [s subordinate? siblings?]
+  (letfn [(fatn [coll res prev]
+            (when-let [a (first coll)]))]))
