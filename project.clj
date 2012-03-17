eb8f79ac1c3b0b65855f28ff413af1c4e66d75d5
Tim McIver <tim@timmciver.com>
2012-03-17 16:36:18 -0400

    Removed marginalia dev-dependency.

--- project.clj
+++ project.clj
@@ -2,5 +2,4 @@
   :description "Some utilities for creating ePUB ebooks."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [enlive "1.0.0-SNAPSHOT"]
-                 [hiccup "1.0.0-beta1"]]
-  :dev-dependencies [[lein-marginalia "0.7.0"]])
+                 [hiccup "1.0.0-beta1"]])
